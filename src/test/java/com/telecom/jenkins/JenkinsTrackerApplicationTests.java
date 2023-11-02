fb1a587a51455f9ce3c19c894b7634c1144aad73
dimodev <morinigodiego21@gmail.com>
2023-11-01 23:43:35 -0300

    initial commit

---
+++ src/test/java/com/telecom/jenkins/JenkinsTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telecom.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
